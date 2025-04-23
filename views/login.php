73b9fe8edbdc8574353c42933ebf5ea7ad646fa9
youcefaddou <youcefaddou@gmail.com>
2025-04-23 11:05:35 +0200

    starting to work on the login feature

---
+++ views/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter</title>
+</head>
+<body>
+<h2 class="form-title">Se connecter</h2>
+        <form class="registerContainer" action="/" method="POST">
+            <div class="form-row">
+                <input type="email" name="mail" placeholder="Email">
+                <input type="password" name="password" placeholder="Mot de passe">
+                <button type="submit" name="register">Se connecter</button>
+            </div>
+        </form>
+</body>
+</html>
