9f5736b60c8e921a560c13dda1e4022bdf9b267f
youcefaddou <youcefaddou@gmail.com>
2025-04-23 12:56:55 +0200

    started a new feature nammed post

--- src/models/repositories/PostRepository.php
+++ src/models/repositories/PostRepository.php
@@ -1,7 +1,19 @@
 <?php
 
 abstract class PostRepository extends Db {
-
+    public static function insertPost($post) {
+        $query = "INSERT INTO posts (title, content, user_id) VALUES (:title, :content, :user_id)";
+        $statement = self::connect()->prepare($query);
+        $statement->bindParam(':title', $post->getTitle());
+        $statement->bindParam(':content', $post->getContent());
+        $statement->bindParam(':user_id', $post->getUserId());
+        
+        if ($statement->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 
